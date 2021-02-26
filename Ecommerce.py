5026f4887eb3f97fcd8a564897c1c15f4e29a1a9
Dhruvita04 <74048191+Dhruvita04@users.noreply.github.com>
2021-02-27 01:08:54 +0530

    Add files via upload

---
+++ Ecommerce.py
@@ -0,0 +1,21 @@
+ ./home/models.py
+
+class Product(Page):
+    sku = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('sku'),
+        FieldPanel('price'),
+        ImageChooserPanel('image'),
+        FieldPanel('short_description'),
+        InlinePanel('custom_fields', label='Custom fields'),
+    ]
