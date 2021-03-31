de2ab04097ba59b70cb18088efadafdd9a8fa3ff
Dhruvita04 <74048191+Dhruvita04@users.noreply.github.com>
2021-03-31 20:04:41 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,15 @@
+# define three sets
+E = {0, 1, 2, 3, 4, 5, 6, 8};
+N = {2, 4};
+
+# set union
+print("Union of E and N is",E | N)
+
+# set intersection
+print("Intersection of E and N is",E & N)
+
+# set difference
+print("Difference of E and N is",E - N)
+
+# set symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
