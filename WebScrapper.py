06493e1f2cc91110c964e0b60f534bf3b0342df0
Dhruv <74048191+Dhruvita04@users.noreply.github.com>
2022-08-09 20:43:23 +0530

    Delete WebScrapper.py

--- WebScrapper.py
+++
@@ -1,8 +0,0 @@
-import requests 
-from bs4 import BeautifulSoup 
-  
-URL = "http://www.values.com/inspirational-quotes"
-r = requests.get(URL) 
-  
-soup = BeautifulSoup(r.content, 'html5lib') 
-print(soup.prettify()) 
